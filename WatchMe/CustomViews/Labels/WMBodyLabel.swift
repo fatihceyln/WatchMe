cc2bd1ebccd73a26abccbfd8fbf4987a470f4ff9
FatihKilit <fatih.kilit@evatro.com>
2022-08-11 18:12:49 +0300

    Custom body label created.

---
+++ WatchMe/CustomViews/Labels/WMBodyLabel.swift
@@ -0,0 +1,36 @@
+//
+//  WMBodyLabel.swift
+//  WatchMe
+//
+//  Created by Fatih Kilit on 11.08.2022.
+//
+
+import UIKit
+
+class WMBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(textAlignment: NSTextAlignment) {
+        self.init(frame: .zero)
+        self.textAlignment = textAlignment
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontForContentSizeCategory = true
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+    }
+}
