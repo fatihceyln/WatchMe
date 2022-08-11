7640da03efe2146bb469f6e284c1b79d129723f3
FatihKilit <fatih.kilit@evatro.com>
2022-08-11 18:10:13 +0300

    Custom title label created.

---
+++ WatchMe/CustomViews/Labels/WMTitleLabel.swift
@@ -0,0 +1,35 @@
+//
+//  WMTitleLabel.swift
+//  WatchMe
+//
+//  Created by Fatih Kilit on 11.08.2022.
+//
+
+import UIKit
+
+class WMTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        self.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+    }
+}
