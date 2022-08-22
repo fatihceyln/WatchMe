697054bf642d5dfad717d98eb1bf35e072e05cbf
FatihKilit <fatih.kilit@evatro.com>
2022-08-22 15:19:15 +0300

    contents in watchlist are now selectable.

--- WatchMe/Screens/WatchlistVC.swift
+++ WatchMe/Screens/WatchlistVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class WatchlistVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class WatchlistVC: WMDataLoadingVC {
     
     private var contents: [ContentDetail] = []
     
@@ -54,7 +54,27 @@ class WatchlistVC: UIViewController, UICollectionViewDelegate, UICollectionViewD
         
         collectionView.pinToEdges(of: view)
     }
-    
+}
+
+extension WatchlistVC {
+    private func getContentDetail(urlString: String, completion: @escaping (ContentDetail?) -> ()) {
+        showLoadingView()
+        
+        NetworkingManager.shared.downloadContentDetail(urlString: urlString) { [weak self] result in
+            guard let _ = self else { return }
+            self?.dismissLoadingView()
+            
+            switch result {
+            case .success(let contentDetail):
+                completion(contentDetail)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+extension WatchlistVC: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         contents.count
     }
@@ -66,5 +86,17 @@ class WatchlistVC: UIViewController, UICollectionViewDelegate, UICollectionViewD
         cell.set(content: contentResult)
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let content = contents[indexPath.row]
+        let urlString = content.isMovie ? ApiUrls.movieDetail(id: content.id?.description ?? "") : ApiUrls.showDetail(id: content.id?.description ?? "")
+        
+        getContentDetail(urlString: urlString) { [weak self] contentDetail in
+            guard let self = self, let contentDetail = contentDetail else { return }
+            DispatchQueue.main.async {
+                self.navigationController?.pushViewController(ContentDetailVC(contentDetail: contentDetail), animated: true)
+            }
+        }
     }
 }
